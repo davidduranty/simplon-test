57b11ff0d2572b83c60d1a8bad91be286bfbed88
David Duranty <dadaduranty@gmail.com>
2024-03-29 20:11:18 +0100

    front: finish project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles/index.scss";
 import axios from "axios";
 
 function App() {
-  const [getData, setGetData] = useState({});
+  const [getData, setGetData] = useState([]);
   const [search, setSearch] = useState("");
 
   const api = {
@@ -17,21 +17,47 @@ function App() {
       const res = await axios.get(
         `${api.base}weather?q=${search}&units=metric&APPID=${api.key}`
       );
-      setGetData(res.data);
-      console.log(res.data);
+      setGetData([res.data]);
+      console.log([res.data]);
     } catch (error) {
       console.error("message: " + error.message);
     }
   };
 
+  // useEffect(() => {
+  //   searchPressed();
+  // }, []);
+
   return (
     <div className="main">
+      <h2>OpenWeather</h2>
       <div className="input-search">
-        <input type="text" onChange={(e) => setSearch(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="City"
+        />
+        <button onClick={searchPressed}>Search</button>
       </div>
-      <h1>{search}</h1>
-      <div className="card-container">{getData.main.temp}</div>
-      <button onClick={searchPressed}>search</button>
+
+      <div className="card-container">
+        {getData.map((data) => (
+          <div key={data.name}>
+            <h1>{data.name}</h1>
+            <p className="city">{data.sys.country}</p>
+            <div className="icon">
+              <img
+                src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                alt={data.weather[0].description}
+              />
+            </div>
+            <p className="condition">
+              Conditions : {data.weather[0].description}
+            </p>
+            <h3>{data.main.temp.toFixed(0)}° C</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
